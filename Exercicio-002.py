ec35ed8a80a15492bbfcfab9aa45bfcab2a4ed14
Lucass_Santana <luucasdsp@hotmail.com>
2025-03-23 20:17:48 -0300

    Verificar as raizes e a unica raiz real.

--- Exercicio-002.py
+++ Exercicio-002.py
@@ -6,3 +6,12 @@ c = -6
 
 delta = b**2 - 4*a*c
 
+if delta > 0:
+    x1 = (-b + math.sqrt(delta)) / (2 * a)
+    x2 = (-b - math.sqrt(delta)) / (2 * a)
+    print(f"As raízes reais são: x1 = {x1} e x2 = {x2}")
+
+elif delta == 0:
+    x = -b / (2 * a)
+    print(f"A única raiz real é: x = {x}")
+    
